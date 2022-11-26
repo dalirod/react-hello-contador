1b138dbfad1295d5d13b8f60cd64631209283b39
Dali <dalirod90@gmail.com>
2022-11-26 23:30:02 +0000

    contador

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,37 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+
+
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+const Home = ({ uno, dos,tres,cuatro,cinco,seis}) =>{
+return (
+<div className="container text-center">
+<div className="row p-5 bg-dark text-white">
+  <div className="reloj col-1 col-sm-1"><FontAwesomeIcon icon={faClock} /></div>
+  <div className="seis display-1 col-2 col-sm-1">{Math.floor(seis % 10)}</div>
+  <div className="cinco display-1 col-2 col-sm-1">{Math.floor(cinco % 10)}</div>
+  <div className="cuatro display-1 col-2 col-sm-1">{Math.floor(cuatro % 10)}</div>
+  <div className="tres display-1 col-2 col-sm-1">{Math.floor(tres % 10)}</div>
+  <div className="dos display-1 col-2 col-sm-1">{Math.floor(dos % 10)}</div>
+  <div className="uno display-1  col-2 col-sm-1">{uno % 10}  </div>
+
+
+  </div>
+	  </div>	
+
+);
+
 };
+		
+
 
 export default Home;
+
+
+
+ 
+	 
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,20 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let contador = 0;
+
+setInterval(() => {
+contador++
+const uno = contador / 1
+const dos = contador / 10
+const tres = contador / 100
+const cuatro = contador / 1000
+const cinco = contador / 10000
+const seis = contador / 100000
+
+    ReactDOM.render(<Home  uno={uno} dos={dos} tres={tres} cuatro={cuatro} cinco={cinco} seis={seis} />, document.querySelector("#app"));
+},1000)
+
+
+
+
